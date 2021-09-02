c214e59e7a5143516eee635b423d357cce3bcd69
BurgosLeo96 <burgosleonard96.lb@gmail.com>
2021-09-01 21:28:32 -0500

    -- Changed argv to receive three more parameters: neighbour size, weights_file and a boolean to store the computed weights (if apply)
    -- Changed random player to store in a csv file the result of each click operation

--- taller2_ML/leo/Minesweeper/Player_Random.py
+++ taller2_ML/leo/Minesweeper/Player_Random.py
@@ -2,25 +2,96 @@
 ## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
 ## =========================================================================
 
-import random, sys
+import numpy as np
+import sys
 from MineSweeperBoard import *
 
 ## -------------------------------------------------------------------------
-if len(sys.argv) < 4:
-    print("Usage: python", sys.argv[0], "width height mines")
+if len(sys.argv) < 5:
+    print("Usage: python", sys.argv[0], "width height mines neighbours_size(8, 24)")
     sys.exit(1)
 # end if
 w = int(sys.argv[1])
 h = int(sys.argv[2])
 m = int(sys.argv[3])
-board = MineSweeperBoard(w, h, m)
+neighbour_size = int(sys.argv[4])
 
-while not board.have_won() and not board.have_lose():
-    print(board)
-    i = random.randint(0, board.width() - 1)
-    j = random.randint(0, board.height() - 1)
-    board.click(j, i)
+neighbours = None
+patches = [[9 for j in range(h)] for i in range(w)]
+
+if neighbour_size == 8:
+    neighbours = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [1, -1],
+        [0, -1]
+    ]
+else:
+    neighbours = [
+        [-2, -2],
+        [-2, -1],
+        [-2, 0],
+        [-2, 1],
+        [-2, 2],
+        [-1, -2],
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [-1, 2],
+        [0, -2],
+        [0, -1],
+        [0, 1],
+        [0, 2],
+        [1, -2],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [2, -2],
+        [2, -1],
+        [2, 0],
+        [2, 1],
+        [2, 2]
+    ]
+
+data = []
+
+for i in range(0, 3):
+    board = MineSweeperBoard(w, h, m)
+    while not board.have_won() and not board.have_lose():
+        print(board)
+
+        data_row = []
+
+        i = int(input("Specify row"))
+        j = int(input("Specify column"))
+        value = board.click(j, i)
+
+        # update patches
+        patches[i][j] = value
+
+        for neighbor in neighbours:
+            neighbour_i = i + neighbor[0]
+            neighbour_j = j + neighbor[1]
+
+            if neighbour_i < 0 or neighbour_j < 0 or neighbour_i >= w or neighbour_j >= h:
+                # TODO: It can be 0 or 9, Try it!
+                data_row.append(0)
+            else:
+                data_row.append(patches[neighbour_i][neighbour_j])
+
+        data_row.append(1 if value != 9 else 0)
+
+        data.append(data_row)
+
 # end while
+
+np_data = np.array(data, dtype=np.int)
+np.savetxt('neighbours_24_2.csv', np_data, delimiter=',')
 
 print(board)
 if board.have_won():
--- taller2_ML/leo/main.py
+++ taller2_ML/leo/main.py
@@ -8,50 +8,90 @@ from Regression.MaximumLikelihood import MaximumLikelihood
 # -------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    if len(sys.argv) < 5:
-        print("Usage: python", sys.argv[0], "width height mines training_file_name")
+    if len(sys.argv) < 8:
+        print("Usage: python", sys.argv[0], "width height mines training_file_name neighbour_size(8, 24) weights_file "
+                                            "(null if training is necessary) store_weights(True-False)")
         sys.exit(1)
     # end if
     w = int(sys.argv[1])
     h = int(sys.argv[2])
     m = int(sys.argv[3])
     training_filename = sys.argv[4]
+    neighbour_size = int(sys.argv[5])
+    weights_filename = sys.argv[6]
+    store_weights = bool(sys.argv[7])
     board = MineSweeperBoard(w, h, m)
 
-    neighbours = [
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, 1],
-        [1, 1],
-        [1, 0],
-        [1, -1],
-        [0, -1]
-    ]
+    neighbours = None
 
-    learning_rate = 1e-4
-    maximum_iterations = 10000
-    epsilon = 1e-8
-    debug_step = 10000
-    init_theta = np.zeros((1, 8))
+    if neighbour_size == 8:
+        neighbours = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1]
+        ]
+    else:
+        neighbours = [
+            [-2, -2],
+            [-2, -1],
+            [-2, 0],
+            [-2, 1],
+            [-2, 2],
+            [-1, -2],
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [-1, 2],
+            [0, -2],
+            [0, -1],
+            [0, 1],
+            [0, 2],
+            [1, -2],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+            [1, 2],
+            [2, -2],
+            [2, -1],
+            [2, 0],
+            [2, 1],
+            [2, 2]
+        ]
 
-    # First board is going to be used to train the model
-    training_board = board.m_Mines
-    training_data = np.loadtxt(fname=training_filename, delimiter=',')
+    weights = None
 
-    x0 = training_data[:, :-1]
-    y0 = training_data[:, -1]
+    if weights_filename != 'null':
+        weights = np.loadtxt(fname=weights_filename, delimiter=',', ndmin=2)
+    else:
+        learning_rate = 1e-4
+        maximum_iterations = 10000
+        epsilon = 1e-8
+        debug_step = 10000
+        init_theta = np.zeros((1, neighbour_size))
 
-    cost = MaximumLikelihood(x0, y0)
+        # First board is going to be used to train the model
+        training_data = np.loadtxt(fname=training_filename, delimiter=',')
 
-    weights, iterations = GradientDescent(
-        cost=cost,
-        learning_rate=learning_rate,
-        init_theta=init_theta,
-        maximum_iterations=maximum_iterations,
-        epsilon=epsilon,
-        debug_step=debug_step
-    )
+        x0 = training_data[:, :-1]
+        y0 = training_data[:, -1]
+
+        cost = MaximumLikelihood(x0, y0)
+        weights, iterations = GradientDescent(
+            cost=cost,
+            learning_rate=learning_rate,
+            init_theta=init_theta,
+            maximum_iterations=maximum_iterations,
+            epsilon=epsilon,
+            debug_step=debug_step
+        )
+
+        if store_weights:
+            weights.tofile('../weights.csv', sep=',')
 
     model = Logistic(weights[0, :-1], weights[-1, -1])
     patches = [[9 for j in range(h)] for i in range(w)]
@@ -76,7 +116,7 @@ if __name__ == '__main__':
 
                         if neighbour_i < 0 or neighbour_j < 0 or neighbour_i >= w or neighbour_j >= h:
                             # TODO: It can be 0 or 9, Try it!
-                            x += [9]
+                            x += [0]
                         else:
                             x += [patches[neighbour_i][neighbour_j]]
 
